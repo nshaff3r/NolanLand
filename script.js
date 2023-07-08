6cd58cd08f0b85fa91ca584499d684cc4be91550
Nolan Shaffer <nshaff3r@nolans-air.lan>
2023-07-08 01:22:19 -0400

    sticky testing

--- script.js
+++ script.js
@@ -5,83 +5,82 @@ $(window).scroll(function(){
     if (height - document.getElementById("title1").offsetTop + 84 <= scrolling)
     {
         var arcade = document.getElementById("arcade");
-        // console.log((window.innerHeight - arcade.offsetHeight) / 2);
         $("#title1").css("visibility", "hidden");
         $("#title2").css("visibility", "visible");
         var arcadeWidth = scrolling - (height - document.getElementById("title1").offsetTop) + 5;
         arcade.style.width = 1.1 * arcadeWidth + "px";
-        if(1.1 * arcadeWidth >= (window.innerWidth * Number(arcade.style.maxWidth.slice(0,2)) / 100))
-        {
-            // $("#scrollbox").css("visibility", "visible");
-        }
-        else
-        {
-            // $("#scrollbox").css("visibility", "hidden");
-        }
     }
     else
     {
         $("#title1").css("visibility", "visible");
         $("#title2").css("visibility", "hidden");
     }   
-  });
+    });
 
+  
+    $(window).resize(screen_size);
 
-  $(window).resize(screen_size);
+    // Adapted from https://stackoverflow.com/questions/44744498/use-of-and-in-javascript
+    function apply_sticky_class(el) {
+        var currentOffset = el.getBoundingClientRect().top;
+        var stickyOffset = parseInt(getComputedStyle(el).top.replace('px', ''));
+        var isStuck = currentOffset <= stickyOffset;
+        el.classList.toggle('js-is-sticky', isStuck);
+    }
 
-  function screen_size()
-  {
-    document.getElementById("title1").innerHTML = `${window.innerWidth}, ${window.innerHeight}`;
-    if (window.innerWidth <= 1050)
+    function screen_size()
     {
-        $("#navigation").css("visibility", "hidden");
-        $("#coaster").css("top", "80px");
-        $("#sidebar").css("width", "100%");
-        $(".menutext").css("font-size", "400%");
-         $(".menutext").css("margin-top", "3vw");
-        $("#home").css("margin-top", "150px");
         var arcade = document.getElementById("arcade");
-        if (window.innerWidth <= 686)
+        apply_sticky_class(arcade);
+        if (window.innerWidth <= 1050)
         {
-            if (window.innerWidth <= 415)
+            $("#navigation").css("visibility", "hidden");
+            $("#coaster").css("top", "80px");
+            $("#sidebar").css("width", "100%");
+            $(".menutext").css("font-size", "400%");
+            $(".menutext").css("margin-top", "3vw");
+            $("#home").css("margin-top", "150px");
+            if (window.innerWidth <= 686)
             {
-                $("#arcade").css("max-width", "65vw");
-                $("#scrollbox").css("width", "46vw");
-                $("#scrollbox").css("height", "25vw");
-                $("#scrollbox").css("left", "27%");
-                $("#scrollbox").css("margin-top", "12vw");
+                if (window.innerWidth <= 415)
+                {
+                    $("#arcade").css("max-width", "65vw");
+                    $("#scrollbox").css("width", "46vw");
+                    $("#scrollbox").css("height", "25vw");
+                    $("#scrollbox").css("left", "27%");
+                    $("#scrollbox").css("margin-top", "12vw");
+                }
+                else
+                {
+                    $("#arcade").css("max-width", "40vw");
+                    $("#scrollbox").css("width", "30vw");
+                    $("#scrollbox").css("height", "16vw");
+                    $("#scrollbox").css("left", "34.5%");
+                    $("#scrollbox").css("margin-top", "0");
+                }
+                $("#arcade").css("top", `${(window.innerHeight - arcade.offsetHeight) / 2}px`);
+                $("#scrollbox").css("top", `${(window.innerHeight - arcade.offsetHeight) / 2 + 190}px`);
             }
             else
             {
                 $("#arcade").css("max-width", "40vw");
-                $("#scrollbox").css("width", "30vw");
-                $("#scrollbox").css("height", "16vw");
-                $("#scrollbox").css("left", "34.5%");
-                $("#scrollbox").css("margin-top", "0");
+                $("#arcade").css("top", "-1px");
+                $("#scrollbox").css("top", "20vw");
             }
-            $("#arcade").css("top", `${(window.innerHeight - arcade.offsetHeight) / 2}px`);
-            $("#scrollbox").css("top", `${(window.innerHeight - arcade.offsetHeight) / 2 + 190}px`);
         }
         else
         {
             $("#arcade").css("max-width", "40vw");
-            $("#arcade").css("top", "0%");
             $("#scrollbox").css("top", "20vw");
+            $("#navigation").css("visibility", "visible");
+            $("#coaster").css("top", "0px");
+            $("#sidebar").css("width", "25vw");
+            $(".menutext").css("font-size", "3.7vw");
+            $(".menutext").css("margin-top", "1vw");
+            $("#home").css("margin-top", "100px");
+            $("#arcade").css("top", "-1px");        
         }
     }
-    else
-    {
-        $("#arcade").css("max-width", "40vw");
-        $("#scrollbox").css("top", "20vw");
-        $("#navigation").css("visibility", "visible");
-        $("#coaster").css("top", "0px");
-        $("#sidebar").css("width", "25vw");
-        $(".menutext").css("font-size", "3.7vw");
-        $(".menutext").css("margin-top", "1vw");
-        $("#home").css("margin-top", "100px");
-        $("#arcade").css("top", "0%");        
-    }
-  }
 
     document.addEventListener('click', printMousePos, true);
     function printMousePos(e){
@@ -90,7 +89,6 @@ $(window).scroll(function(){
             let cursorY= e.pageY;
             console.log( "pageX: " + cursorX +",pageY: " + cursorY );
     }
-
     
     document.addEventListener("DOMContentLoaded", function()
     {
